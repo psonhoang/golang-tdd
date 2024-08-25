fcad1775836a7daccaa6b169372a7274e46ae9c8
Hoang S Pham <pshoang102@gmail.com>
2024-08-25 01:11:26 -0500

    readseeker for file storage system and record win

--- application/file_system_store.go
+++ application/file_system_store.go
@@ -6,11 +6,37 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadWriteSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
-	var league []Player
-	json.NewDecoder(f.database).Decode(&league)
+	f.database.Seek(0, io.SeekStart)
+	league, _ := NewLeague(f.database)
 	return league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+
+	for i, player := range league {
+		if player.Name == name {
+			league[i].Wins++
+		}
+	}
+
+	f.database.Seek(0, io.SeekStart)
+	json.NewEncoder(f.database).Encode(league)
 }
